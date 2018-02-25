0ca54d06653aced14cc5c8d7f07f51d1509b7ea3
W01fpaw <wsarchett@comcast.net>
2018-02-24 21:44:19 -0800

    Created named routes and added PagesController, thank you page, and general fixes

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    function home() {
+        return view('pages.index');
+    }
+
+    function about() {
+        return view('pages.about');
+    }
+
+    function contact() {
+        return view('pages.contact');
+    }
+
+    function store(Request $request) {
+
+        $name = $request->name;
+
+        return redirect()->route('thanks',['name' => $name]);
+    }
+
+    function thanks($name) {
+        return view('pages.thankyou')->with(compact('name'));
+    }
+
+    function create_account() {
+        return view('pages.create_account');
+    }
+
+    function log_in() {
+        return view('pages.log_in');
+    }
+}
